4901feb1246ccd06bb71104f09effd01b8b58dc9
LAMIYA16 <lamiyafsal16@gmail.com>
2025-07-31 19:03:05 +0530

    budget added to signup features

--- templates/script.js
+++ templates/script.js
@@ -1,6 +1,9 @@
 // Placeholder for future JS logic
 console.log("Planora homepage loaded.");
 
+console.log("Selected Budget:", selectedBudget);
+
+
 // Placeholder for future interactivity
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Planora features section loaded");
